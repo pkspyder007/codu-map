87529174976bbc5dd9731928fd7ac92790341460
Praveen Kumar <pkspyder007@gmail.com>
2020-11-01 00:53:10 +0530

    INITIAL COMMIT

---
+++ src/members.js
@@ -0,0 +1,11 @@
+export const members = [
+  {
+    githubUsername: "pkspyder007",
+    name: "Praveen Kumar",
+    location: "ETAH, UTTAR PRADESH, INDIA",
+    position: {
+      lat: 27.56848,
+      lng: 78.64682,
+    },
+  },
+];
