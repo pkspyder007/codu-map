c0b3c5bb6bb6e15970cfea228ae00eb0529a16be
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-07-15 10:44:25 +0300

    added markers for members

--- src/components/NewMap.js
+++ src/components/NewMap.js
@@ -21,32 +21,33 @@ function Map() {
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={2.4}
+          zoom={2.6}
         >
           {members.map((member, idx) => (
-          <Marker
-            key={idx}
-            position={member.position}
-            title={member.githubUsername}
-            name={member.name}
-            //onClick={onMarkerClick} onMarkerClick is undefined
-            // name={`${member.name}'s Location`}
-          >
-            { <InfoWindow //You must provide either an anchor (typically render it inside a <Marker>) or a position props for <InfoWindow>.
-              //marker={this.state.activeMarker}
-              //visible={this.state.showingInfoWindow}
+            <Marker
+              key={idx}
+              position={member.position}
+              title={member.githubUsername}
+              name={member.name}
+              //onClick={onMarkerClick} onMarkerClick is undefined
+              // name={`${member.name}'s Location`}
             >
-              <div>
-                <img
-                  alt={`User Icon for ${member.name}`}
-                  src={`https://github.com/${member.githubUsername}.png`}
-                  className="UserCard__image"
-                />
-                <p>{member.location}</p>
-              </div>
-            </InfoWindow> }
-          </Marker>
-        ))}
+              {/* <InfoWindow //You must provide either an anchor (typically render it inside a <Marker>) or a position props for <InfoWindow>.
+                //marker={this.state.activeMarker}
+                //visible={this.state.showingInfoWindow}
+                  anchor={0}
+                >
+                <div>
+                  <img
+                    alt={`User Icon for ${member.name}`}
+                    src={`https://github.com/${member.githubUsername}.png`}
+                    className="UserCard__image"
+                  />
+                  <p>{member.location}</p>
+                </div>
+              </InfoWindow> */}
+            </Marker>
+          ))}
         </GoogleMap>
       </LoadScript>
     </div>
