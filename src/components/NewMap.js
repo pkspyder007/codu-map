50294b46ab5c7534322808c8fbf93456f36c5ab8
BrancuAlexandru <alexandrubrancu@yahoo.com>
2022-07-18 08:14:20 +0300

    map is now fully functional

--- src/components/NewMap.js
+++ src/components/NewMap.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoogleMap, LoadScript, InfoWindow, Marker } from '@react-google-maps/api';
 import members from "../members";
 
@@ -12,84 +12,54 @@ const center = {
   lng: 10
 };
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {}
-    };
-  }
+const Map = () => {
+  const [activeMarker, setActiveMarker] = useState(null);
 
-  handleMarkerClick = (member) => {
-    return (
-      <InfoWindow
-        key={member.id}
-        visible={this.state.showingInfoWindow}
-        position={member.position}
-        onCloseClick={() => this.handleInfoWindowClose(member.name)}
-        onLoad={() => {
-          this.setState ({
-            showingInfoWindow: true,
-            openInfoWindowId: member.id
-          })
-        }}
-      >
-        <div>
-          <img
-            alt={`User Icon for ${member.name}`}
-            src={`https://github.com/${member.githubUsername}.png`}
-            className="UserCard__image"
-          />
-          <p>{member.location}</p>
-        </div>
-      </InfoWindow>
-    )
-  }
-
-  handleInfoWindowClose = (name) => {
-    if (this.state.showingInfoWindow && this.state.activeMarker.name === name) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
     }
-  }
-
-  handleMapClicked = () => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: {}
-      });
-    }
+    setActiveMarker(marker);
   };
-  render() {
-    return (
-      <div className="map">
-        <LoadScript googleMapsApiKey="AIzaSyBpIwkBrzchs9ICE4tdERb-8tuZV1R8yl8">
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={center}
-            zoom={2.6}
-            onClick={this.handleMapClicked}
-          >
-            {members.map((member, id) => (
-              <Marker
-                key={id}
-                position={member.position}
-                title={member.githubUsername}
-                name={member.name}
-                onClick={this.handleMarkerClick(member)}
-                // name={`${member.name}'s Location`}
-              >
-              </Marker>
-            ))}
-          </GoogleMap>
-        </LoadScript>
-      </div>
-    )
-  }
+  return (
+    <div className="Map">
+      <LoadScript googleMapsApiKey="AIzaSyBpIwkBrzchs9ICE4tdERb-8tuZV1R8yl8">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={2.6}
+          onClick={() => setActiveMarker(null)}
+        >
+          {members.map((member, id) => (
+            <Marker
+              key={id}
+              position={member.position}
+              title={member.githubUsername}
+              name={member.name}
+              onClick={() => handleActiveMarker(id)}
+            >
+              {activeMarker === id ? (
+                <InfoWindow
+                  key={id}
+                  position={member.position}
+                  onCloseClick={() => setActiveMarker(null)}
+                >
+                  <div>
+                    <img
+                      alt={`User Icon for ${member.name}`}
+                      src={`https://github.com/${member.githubUsername}.png`}
+                      className="UserCard__image"
+                    />
+                    <p>{member.location}</p>
+                  </div>
+                </InfoWindow>
+              ) : null}
+            </Marker>
+          ))}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  )
 }
 
 export default Map;
